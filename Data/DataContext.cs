76dd06fdd7941f37f83674753991bbfb388a5fc9
brunoferreira03 <bff3rreira03@gmail.com>
2023-05-24 17:19:38 +0100

    First Controller

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using SuperShopBrunoFerreira.Data.Entity;
+
+namespace SuperShopBrunoFerreira.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) :base(options)
+        {
+
+        }
+    }
+}
